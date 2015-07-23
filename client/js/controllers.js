82d6a07b27672d05ae6aa0bd5c322b8fa5c92a4c
Brian van Vlymen <dontgizeup@yahoo.com>
2015-07-23 16:14:18 +0000

    second commit

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -19,7 +19,7 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
     
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
     
     $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     
